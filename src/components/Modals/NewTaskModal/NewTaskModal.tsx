de2b662cbd4325b776b4c9473a60fcdb5c1c2ed8
vbybyk <vitaliy.bybyk@gmail.com>
2022-12-10 17:50:29 +0800

    Improved modal form inputs: Select

--- src/components/Modals/NewTaskModal/NewTaskModal.tsx
+++ src/components/Modals/NewTaskModal/NewTaskModal.tsx
@@ -1,13 +1,15 @@
-import { Input, Modal, Button, Divider } from "antd";
+import { Input, Modal, Button, Divider, Select, Tag } from "antd";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import type { CustomTagProps } from "rc-select/lib/BaseSelect";
+import { requiredFieldMessage } from "../../Common/Constants/Constants";
 import "./newTaskModal.scss";
 
 interface IFormInputs {
   name: string;
   description: string;
-  label: string;
+  label: [];
   priority: string;
 }
 
@@ -18,12 +20,52 @@ interface IProps {
 
 const { TextArea } = Input;
 
+const labelOptions = [
+  { value: "gold", label: "FRONTEND" },
+  { value: "lime", label: "DESIGN" },
+  { value: "green", label: "BACKEND" },
+  { value: "cyan", label: "INVESTIGATE" },
+];
+
+const priorityOptions = [
+  { value: "low", label: "Low" },
+  { value: "medium", label: "Medium" },
+  { value: "high", label: "High" },
+];
+
 const schema = yup
   .object({
-    name: yup.string().max(50).required(),
-    description: yup.string().max(150).required(),
+    name: yup
+      .string()
+      .min(2, "Please, add at least 2 symbols")
+      .max(150, "Please, make it shorter than 50 symbols")
+      .required(requiredFieldMessage),
+    description: yup
+      .string()
+      .min(2, "Please, add at least 2 symbols")
+      .max(999, "Please, make it shorter than 1000 symbols")
+      .required(requiredFieldMessage),
   })
   .required();
+
+const tagRender = (props: CustomTagProps) => {
+  const { label, value, closable, onClose } = props;
+  const onPreventMouseDown = (event: React.MouseEvent<HTMLSpanElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+  return (
+    <Tag
+      color={value}
+      onMouseDown={onPreventMouseDown}
+      closable={closable}
+      onClose={onClose}
+      style={{ marginRight: 3 }}
+    >
+      {label}
+    </Tag>
+  );
+};
 
 export const NewTaskModal = (props: IProps) => {
   const { isModalOpen, setIsModalOpen } = props;
@@ -38,7 +80,7 @@ export const NewTaskModal = (props: IProps) => {
     defaultValues: {
       name: "",
       description: "",
-      label: "",
+      label: [],
       priority: "",
     },
     resolver: yupResolver(schema),
@@ -60,40 +102,74 @@ export const NewTaskModal = (props: IProps) => {
   };
 
   return (
-    <Modal title="New Task" open={isModalOpen} className="new-task-modal" onCancel={handleCancel} footer={null}>
+    <Modal
+      title="New Task"
+      open={isModalOpen}
+      className="new-task-modal"
+      onCancel={handleCancel}
+      width={700}
+      footer={null}
+    >
       <form onSubmit={handleSubmit(onSubmit)} className="new-task-modal__form">
+        <p className="input-field-title">Task name</p>
         <Controller
           name="name"
           control={control}
-          render={({ field }) => <Input placeholder="Task name CONTROLLER" style={{ width: 200 }} {...field} />}
+          render={({ field }) => <Input placeholder="Task name" {...field} />}
         />
-        {/* <Input placeholder="Task name" style={{ width: 200 }} {...register("name")} /> */}
-        <p>{errors.name?.message}</p>
+        <p className="required-field-message">{errors.name?.message}</p>
+
+        <p className="input-field-title">Description</p>
         <Controller
           name="description"
           control={control}
           render={({ field }) => (
             <TextArea
-              placeholder="Autosize height with minimum and maximum number of lines"
-              autoSize={{ minRows: 2, maxRows: 2 }}
+              placeholder="Describe your task"
+              autoSize={{ minRows: 3, maxRows: 20 }}
+              maxLength={1000}
+              showCount={true}
               {...field}
             />
           )}
         />
-        {/* <TextArea
-          placeholder="Autosize height with minimum and maximum number of lines"
-          autoSize={{ minRows: 2, maxRows: 6 }}
-          {...register("description")}
-        /> */}
-        <p>{errors.description?.message}</p>
+        <p className="required-field-message">{errors.description?.message}</p>
 
-        <Input placeholder="Label" style={{ width: 150 }} {...register("label")} />
-        <Input placeholder="Priority" style={{ width: 150 }} {...register("priority")} />
+        <p className="input-field-title">Label</p>
+        <Controller
+          name="label"
+          control={control}
+          render={({ field }) => (
+            <Select
+              mode="multiple"
+              showArrow
+              tagRender={tagRender}
+              defaultValue={[]}
+              style={{ width: "100%" }}
+              options={labelOptions}
+              {...field}
+              optionFilterProp="label"
+              labelInValue
+            />
+          )}
+        />
+
+        {/* <Input placeholder="Label" style={{ width: 150 }} {...register("label")} /> */}
+
+        <p className="input-field-title">Priority</p>
+        <Controller
+          name="priority"
+          control={control}
+          render={({ field }) => <Select showArrow style={{ width: "150px" }} options={priorityOptions} {...field} />}
+        />
+        {/* <Input placeholder="Priority" style={{ width: 150 }} {...register("priority")} /> */}
 
         <Divider />
         <div className="new-task-modal__buttons-wrapper">
           <Button onClick={handleCancel}>Cancel</Button>
-          <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
+          <Button type="primary" onClick={handleSubmit(onSubmit)} style={{ marginLeft: "20px" }}>
+            Submit
+          </Button>
         </div>
       </form>
     </Modal>
