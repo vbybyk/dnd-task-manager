e9af9dbaec0fb4c987231b08f25603ade27d96ca
vbybyk <vitaliy.bybyk@gmail.com>
2022-12-19 12:29:17 +0800

    Server: add mongo + project/task Schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,24 @@
 import express, {Application} from "express";
+import mongoose from 'mongoose';
+import { ProjectModel } from "./models/ProjectSchema";
  
 const port = 9000;
 
 const mount = async (app: Application) => {
 
+    (await mongoose.connect("mongodb+srv://vbybyk:mongovbybyk21@cluster0.antv1.mongodb.net/dnd?retryWrites=true&w=majority"))
     // app.use(express.static(`${__dirname}/client`));
     // app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
 
-app.get('/', (_req, res) => {
-    res.send('hi there common man man!')
-})
+    app.get('/projects', async (_req, res) => {
+        await ProjectModel.find({}, (err: any, data: any) => {
+            if(err) {
+                res.send(err)
+            } else {
+                res.send(data)
+            }
+        }).clone()
+    })
 
     // const startServer = async() => {
     //     await server.start();
