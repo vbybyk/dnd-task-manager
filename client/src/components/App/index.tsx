c01e08fd616a7c3765fab74b3f30842dac45e16a
vbybyk <vitaliy.bybyk@gmail.com>
2024-05-18 15:14:35 +0800

    Updated global font-family

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ConfigProvider } from "antd";
 import { HeaderApp, Dashboard, Project } from "../index";
 import { AppLayout } from "./AppLayout/AppLayout";
-import { Provider } from "react-redux";
 import Toaster from "../Common/Toaster/Toaster";
 import store from "../store";
 import useTasks from "../Hooks/useTasks";
@@ -33,7 +34,16 @@ const APP: React.FC = () => {
 export const App: React.FC = (props: any) => {
   return (
     <Provider store={store}>
-      <APP {...props} />
+      <ConfigProvider
+        theme={{
+          hashed: false,
+          token: {
+            fontFamily: "Rubik, sans-serif",
+          },
+        }}
+      >
+        <APP {...props} />
+      </ConfigProvider>
     </Provider>
   );
 };
