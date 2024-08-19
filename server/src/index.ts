7a74f9b55b7bb6f49f8e1855122434716333c188
vbybyk <vitaliy.bybyk@gmail.com>
2024-08-19 21:43:10 +0800

    Commented express static redirect to client

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,12 +38,12 @@ const mount = async (app: Application) => {
   app.listen(port);
   console.log(`App is listening on port ${port}`);
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-    });
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, "../client/dist")));
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+  //   });
+  // }
 };
 
 mount(express());
